19c150764fd516ca73869d94a5060423a5b8d1ff
unknown <devritim@mail.gvsu.edu>
2020-08-24 15:04:15 -0400

    Fixed routes

--- src/app/pages/article-view/article-references.ts
+++ src/app/pages/article-view/article-references.ts
@@ -5,8 +5,8 @@ import { IframeNestedWebsiteComponent } from '../programming/programming-pages/i
 import { DynamicComponentsComponent } from '../programming/programming-pages/dynamic-components/dynamic-components.component';
 
 export const ARTICLE_REFERENCES: Map<string, ArticleReference> = new Map([
-    ['sticky-footer', {pageTitle: 'Angular Sticky Footer Example', pagePath: 'angular-sticky-footer-example', parentPath: '/programming', component: StickyFooterComponent}],
-    ['dynamic-components', {pageTitle: 'Angular Dynamically Loaded Components', pagePath: 'angular-dynamically-loaded-components', parentPath: '/programming', component: DynamicComponentsComponent}],
-    ['dark-theme', {pageTitle: 'Angular Dark and Light Themes With Toggle', pagePath: 'angular-dark-and-light-themes-with-toggle', parentPath: '/programming', component: DarkThemeComponent}],
-    ['iframe-nested-website', {pageTitle: 'Angular "Website Within a Website" Using IFrame', pagePath: 'angular-website-within-a-website-using-iframe', parentPath: '/programming', component: IframeNestedWebsiteComponent}]
+    ['sticky-footer', {pageTitle: 'Angular Sticky Footer Example', pagePath: 'angular-sticky-footer-example', parentPath: 'programming', component: StickyFooterComponent}],
+    ['dynamic-components', {pageTitle: 'Angular Dynamically Loaded Components', pagePath: 'angular-dynamically-loaded-components', parentPath: 'programming', component: DynamicComponentsComponent}],
+    ['dark-theme', {pageTitle: 'Angular Dark and Light Themes With Toggle', pagePath: 'angular-dark-and-light-themes-with-toggle', parentPath: 'programming', component: DarkThemeComponent}],
+    ['iframe-nested-website', {pageTitle: 'Angular "Website Within a Website" Using IFrame', pagePath: 'angular-website-within-a-website-using-iframe', parentPath: 'programming', component: IframeNestedWebsiteComponent}]
 ]);
